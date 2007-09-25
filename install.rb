88737f5aeb3e11b2148492d4a1b5f200e196f3da
Brian Hogan <brianhogan@napcs.com>
2007-09-25 00:31:59 +0000

    Unified installer for all rails versions

--- install.rb
+++ install.rb
@@ -2,6 +2,7 @@
 # Prompts users for the desired source base path
 # and writes the path to the configuration file used
 # by the plugin.
+require 'FileUtils'
 puts "########################################################"
 puts "# reverse_proxy_fix plugin                             #"
 puts "# Set up the base url of your proxy server             #"
@@ -12,13 +13,24 @@ base_url = STDIN.gets
 
 base_url.chop!
 
+puts "Please select your Rails version from the list of supported versions:"
+puts " 1 : Rails 1.1.6"
+puts " 2 : Rails 1.2, 1.2.2, and 1.2.3"
+version = STDIN.gets.chop!
+
 d = Dir.getwd
 
-if !d.include?('/vendor/plugins/reverse_proxy_fix')
-  d += '/vendor/plugins/reverse_proxy_fix'
-elsif !d.include?('/vendor/plugins/')
-  
+#if !d.include?('/vendor/plugins/reverse_proxy_fix')
+#  d += '/vendor/plugins/reverse_proxy_fix'
+#elsif !d.include?('/vendor/plugins/')
+#  
+#end
+#config_file = d + "/lib/config.rb"
+
+if File.exists?("app")
+   d += "/vendor/plugins/reverse_proxy_fix"
 end
+
 config_file = d + "/lib/config.rb"
 
 begin
@@ -28,4 +40,28 @@ begin
   puts "The file #{config_file} has been modified. The plugin will be activated when you run your application in production mode."
 rescue
   puts "I couldn't find your config file. I'm looking for a file called 'config.rb' and it should reside in the /lib file of the plugin. Since I can't find it, you may want to just open it yourself and change the value for BASE_PATH. Be sure that line is uncommented!!!!!!"
+  
 end
+
+
+if version == 1
+   version = "116"
+elsif version == 2
+   version = "123"
+else
+   version = "123"
+end
+
+
+config_file = d + "/lib/#{version}.rb"
+
+begin
+FileUtils.cp "#{d}/lib/#{version}.rb", "#{d}/lib/reverse_proxy_fix.rb"
+puts "Plugin successfully installed and configured"
+rescue
+    puts "I couldn't copy your file.  Remane #{d}/lib/#{version}.rb to #{d}/lib/reverse_proxy_fix.rb"
+end
+
+
+
+
---
+++ lib/116.rb
@@ -0,0 +1,45 @@
+# Reverse proxy filx for Rails 1.1.6 and previous applications
+module ActionController
+		
+	protected
+	# Configure the prefix on the url only if we're running in production mode
+	# Throws an exception if the BASE_URL constant has not been configured in
+	# config.rb
+	def self.check_mode_and_base
+		if RAILS_ENV == 'production'
+			begin
+				BASE_URL
+			rescue
+				raise "You need to configure your base url. See the reverse_proxy_fix/lib/config.rb file to set this value!"
+			end
+		else
+			return false
+		end
+	end
+	
+	# Set the asset host for CSS, JS, and image files if we're in production mode and the base_path has been configured.
+	if check_mode_and_base
+		ActionController::Base.asset_host = BASE_URL
+	end
+	
+	class UrlRewriter
+	
+		alias old_rewrite_url rewrite_url
+		
+		# Prepends the BASE_URL to all of the URL requests created by the 
+		# URL rewriter in Rails.
+		def rewrite_url(path, options)
+			url = old_rewrite_url(path, options)
+			if ActionController::check_mode_and_base
+			  unless options[:skip_relative_url_root]
+					
+			    url = url.gsub(@request.protocol + @request.host_with_port, '')
+			    url = BASE_URL + url
+			  end
+			end
+			url
+		
+		end
+	end
+
+end
