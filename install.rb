c70e9ba2033c8286d9d89b6b5aac3e6ed309e17c
Dave Nolan <dave@textgoeshere.org.uk>
2009-05-04 19:40:42 +0800

    Updated installation, README
    Signed-off-by: Brian Hogan / New Auburn Personal Computer Services <bphogan@gmail.com>

--- install.rb
+++ install.rb
@@ -4,15 +4,10 @@
 # by the plugin.
 require 'fileutils'
 puts "########################################################"
-puts "# reverse_proxy_fix plugin - v1.0.6.0                  #"
+puts "# reverse_proxy_fix plugin - v1.1                      #"
 puts "# Configureation                                       #"
 puts "########################################################"
 puts ""
-puts "Enter the base url you wish to use without the trailing slash (example: http://external.mycompany.com/myapp)"
-base_url = STDIN.gets
-
-base_url.chop!
-
 puts "Please select your Rails version from the list of supported versions:"
 puts " 1 : Rails 1.1.6"
 puts " 2 : Rails 1.2.X"
@@ -22,44 +17,51 @@ version = STDIN.gets.chop!.to_i
 
 d = Dir.getwd
 
-
-if File.exists?("app")
-   d += "/vendor/plugins/reverse_proxy_fix"
+version_file = case version
+  when 1
+    "116"
+  when 2
+    "123"
+  when 3
+    "20"
+  when 4
+    "23"
+  else 
+    "123"
 end
 
-config_file = d + "/lib/config.rb"
-
-begin
-  File.open(config_file, "w") do |t|
-  	t << "BASE_URL=\"#{base_url}\""
-  end	
-  puts "The file #{config_file} has been modified. The plugin will be activated when you run your application in production mode."
-rescue
-  puts "I couldn't find your config file. I'm looking for a file called 'config.rb' and it should reside in the /lib file of the plugin. Since I can't find it, you may want to just open it yourself and change the value for BASE_PATH. Be sure that line is uncommented!!!!!!"
+if version < 4
+  puts "Enter the base url you wish to use without the trailing slash (example: http://external.mycompany.com/myapp)"
+  base_url = STDIN.gets
   
-end
-
-
-if version == 1
-   version = "116"
-elsif version == 2
-   version = "123"
-elsif version == 3
-   version = "20"
-elsif version == 4
-   version = "23"
+  base_url.chop!
+  
+  
+  if File.exists?("app")
+     d += "/vendor/plugins/reverse_proxy_fix"
+  end
+  
+  config_file = d + "/lib/config.rb"
+  
+  begin
+    File.open(config_file, "w") do |t|
+      t << "BASE_URL=\"#{base_url}\""
+    end	
+    puts "The file #{config_file} has been modified. The plugin will be activated when you run your application in production mode."
+  rescue
+    puts "I couldn't find your config file. I'm looking for a file called 'config.rb' and it should reside in the /lib file of the plugin. Since I can't find it, you may want to just open it yourself and change the value for BASE_PATH. Be sure that line is uncommented!!!!!!"
+  end
 else
-   version = "123"
+  puts "Add BASE_URL to any environment conf to enable reverse_proxy_fix"
 end
 
-
-config_file = d + "/lib/#{version}.rb"
+config_file = d + "/lib/#{version_file}.rb"
 
 begin
-  FileUtils.cp "#{d}/lib/#{version}.rb", "#{d}/lib/reverse_proxy_fix.rb"
+  FileUtils.cp "#{d}/lib/#{version_file}.rb", "#{d}/lib/reverse_proxy_fix.rb"
   puts "Plugin successfully installed and configured"
 rescue
-    puts "I couldn't copy your file.  Remane #{d}/lib/#{version}.rb to #{d}/lib/reverse_proxy_fix.rb"
+    puts "I couldn't copy your file.  Remane #{d}/lib/#{version_file}.rb to #{d}/lib/reverse_proxy_fix.rb"
 end
 
 
